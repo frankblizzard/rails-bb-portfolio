e84e3ca0b2a8cefd1da0def975be19777d7ca35f
Thomas Maximini <thomas.maximini@medx.net>
2012-12-06 17:28:35 +0100

    added rails 3.2.8

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBbPortfolio::Application.config.secret_token = '43e7d5eea634353a76be0db296d8db72150c1ecb86f124c71ad3e6012b3ae887863b0692032ceb48e812551ac6e8e92d2e167696c5a48912d4a38eef436536c5'
